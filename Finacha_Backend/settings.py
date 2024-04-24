b99b620beaeb7ba9ae4363ed5d13a556c39a778d
BossEmma <128752098+BossEmma@users.noreply.github.com>
2024-04-24 11:48:23 +0100

    Update settings.py

--- Finacha_Backend/settings.py
+++ Finacha_Backend/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Finacha_Backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-#STATICFILES_DIRS=[os.path.join(BASE_DIR , "static")]
+STATICFILES_DIRS=[os.path.join(BASE_DIR , "static")]
 
 STATIC_ROOT= BASE_DIR/"staticfiles"
 
